96c7f980583a9661486499166d0925d547162301
Praful Mohanan <praful.mohanan@gmail.com>
2020-01-06 14:50:10 +0530

    Configured User Defined Models

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from userapp.models import User
 # Register your models here.
+
+ADDITIONAL_USER_FIELDS = (
+    (None, {'fields': ('is_consumer','is_provider')}),
+)
+
+class MyUserAdmin(UserAdmin):
+    model = User
+
+    add_fieldsets = UserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
+    fieldsets = UserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
+
+admin.site.register(User,MyUserAdmin)
--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_consumer = models.BooleanField('consumer status',default=False)
+    is_provider = models.BooleanField('provider status',default=False)
+    def __str__(self):
+        return self.username
