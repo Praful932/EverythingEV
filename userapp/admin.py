9f144a0fec5df837147f6410a1b5e37e4c9226f2
Praful Mohanan <praful.mohanan@gmail.com>
2020-01-06 15:54:20 +0530

    Added Consumer,Provider Models

--- userapp/admin.py
+++ userapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from userapp.models import User
+from userapp.models import User, Consumer, Provider
 # Register your models here.
 
+admin.site.register(Consumer)
+admin.site.register(Provider)
 ADDITIONAL_USER_FIELDS = (
     (None, {'fields': ('is_consumer','is_provider')}),
 )
--- userapp/models.py
+++ userapp/models.py
@@ -7,3 +7,20 @@ class User(AbstractUser):
     is_provider = models.BooleanField('provider status',default=False)
     def __str__(self):
         return self.username
+
+class Consumer(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE, primary_key = True)
+    have_vehicle = models.BooleanField('Have Vehicle',default = False)
+
+class Provider(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE, primary_key = True)
+    lat = models.DecimalField(max_digits=9,decimal_places=6,blank=True,null=True)
+    lng = models.DecimalField(max_digits=9,decimal_places=6,blank=True,null=True)
+    business_name = models.CharField(max_length = 100)
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length = 100)
+    company = models.CharField(max_length = 100)
+    range = models.CharField(max_length = 100)
+    battery_capacity = models.CharField(max_length = 100)
+    
