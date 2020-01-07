61af06409550f90c6128912a9caf5aafd169cd37
akankshast <akankshatamboli195@gmail.com>
2020-01-07 14:34:33 +0530

    vehicle data for consumer n provider

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = 'index'),
-    path('stations/',views.stations, name='stations')
+    path('stations/',views.stations, name='stations'),
+    path('vehicledata_c/',views.vehicledata_c, name='vehicledata_c'),
+    path('vehicledata_p/',views.vehicledata_p, name='vehicledata_p'),
+
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -4,5 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,"userapp/index.html")
+
 def stations(request):
     return render(request, "userapp/stations.html")
+
+def vehicledata_c(request):
+    return render(request, "userapp/vehicledata_c.html")
+def vehicledata_p(request):
+    return render(request, "userapp/vehicledata_p.html")
