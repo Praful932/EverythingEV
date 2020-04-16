26e0fc715aa5727c7a1ed38cd7c448cb2988fc4c
Abhishek Ghadge <abhyag126@gmail.com>
2020-04-16 21:09:09 +0530

    cards and analytics latest commit

--- userapp/urls.py
+++ userapp/urls.py
@@ -26,15 +26,11 @@ urlpatterns = [
     path('Charging-Station/all-stations/',views.ChargingStationConsumer, name = 'Charging-Station-CLV'),
     path('Charge-Pooling/',views.ChargePooling,name = 'Charge-Pooling'),
     path('Route-Your-Way/',views.RouteYourWay, name = 'Route-Your-Way'),
-<<<<<<< HEAD
-    path('dashboard/',dash.as_view(),name='dashboard')
-] 
-=======
-    path('Maintenance-man/dashboard', views.MaintenanceDashboard, name = 'Maintenance-man-dashboard'),
+    path('dashboard/',dash.as_view(),name='dashboard'),
+   path('Maintenance-man/dashboard', views.MaintenanceDashboard, name = 'Maintenance-man-dashboard'),
     path('Maintenance-man/view-all', views.AllMaintenanceMan, name = 'All-Maintenance-Man'),
     path('Maintenance-man/complaints', views.PendingComplaints, name = 'Complaint-Dashboard'),
     ] 
->>>>>>> 25c3c48cd825375f27c2c0cd76f6690ca56282f9
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
