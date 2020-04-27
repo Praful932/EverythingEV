3c0fc12660040e5e44008f3b1f39cd661c5cff1c
akankshast <akankshatamboli195@gmail.com>
2020-04-27 22:25:49 +0530

    chargepooling and password pages UI done

--- Sih/settings.py
+++ Sih/settings.py
@@ -184,5 +184,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS_EV')
+EMAIL_HOST_USER = os.environ.get('TEMP_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('TEMP_PASS')
