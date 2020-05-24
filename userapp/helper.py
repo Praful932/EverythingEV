9c238babf3e2474e7d442e02ca584a20bd01c566
Praful <praful.mohanan@gmail.com>
2020-05-24 23:27:03 +0530

    Add Data Hidden form

---
+++ userapp/helper.py
@@ -0,0 +1,18 @@
+import json
+import math
+from urllib.request import urlopen
+
+
+def get_user_location():
+    """
+    Return value:
+    Tuple - (latitude_user, longitude_user)
+    """
+    url = 'http://ipinfo.io/json'
+    response = urlopen(url)
+    data = json.load(response)
+    # Get User location(lat & lng)
+    lat_user = math.radians(float(data['loc'].split(',')[0]))
+    lng_user = math.radians(float(data['loc'].split(',')[1]))
+
+    return (lat_user, lng_user)
