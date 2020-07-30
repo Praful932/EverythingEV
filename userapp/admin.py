7ac40c20ef1607b3117f78cd80f44dae1e3903f2
Praful <praful.mohanan@gmail.com>
2020-07-30 19:58:42 +0530

    Add User Import Export

--- userapp/admin.py
+++ userapp/admin.py
@@ -59,12 +59,12 @@ ADDITIONAL_USER_FIELDS = (
     (None, {'fields': ('is_consumer', 'is_provider')}),
 )
 
-
-class MyUserAdmin(UserAdmin):
+@admin.register(User)
+class MyUserAdmin(UserAdmin,ImportExportModelAdmin):
     model = User
 
     add_fieldsets = UserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
     fieldsets = UserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
 
 
-admin.site.register(User, MyUserAdmin)
+# admin.site.register(User, MyUserAdmin)
